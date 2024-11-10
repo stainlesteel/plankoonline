41ddebe2d127e65e713814b7a1cf50439e112761
heqingbao <qingbao.ho@gmail.com>
2024-11-11 00:42:53 +0800

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,16 +4,18 @@
 </script>
 
 <svelte:head>
-  <title>Plinko</title>
+  <title>Free Plinko Game Online - Play & Win Instant Rewards</title>
+  <description>Experience authentic Plinko gameplay online! Drop discs through pegs for exciting rewards in this classic arcade game. No download or registration required - start playing instantly and win.</description>
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+  <link rel="canonical" href="https://plinko-game-online.github.io/"/>
   <link
     href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
     rel="stylesheet"
   />
   <meta property="og:type" content="website" />
-  <meta property="og:title" content="Plinko" />
-  <meta property="og:url" content="https://plinko-web-game.netlify.app/" />
+  <meta property="og:title" content="Free Plinko Game Online" />
+  <meta property="og:url" content="https://plinko-game-online.github.io/" />
   <meta property="og:image" content={ogImage} />
 </svelte:head>
 
